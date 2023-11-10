6009a32b23f3bf29b0e370343f82be695e282dbe
Alcex <alcexn@163.com>
2023-11-11 02:40:32 +0800

    Update update.js

--- update.js
+++ update.js
@@ -1,40 +1,38 @@
-/**
- * 更新 package.json 版本号
- */
-const fetch = require("node-fetch");
-const md5 = require("js-md5");
-const fs = require("fs");
+import fetch from 'node-fetch';
+import md5 from 'js-md5';
+import fs from 'fs/promises';
 
-const getVersion = async() => {
-    const latestVersion = await fetch("https://registry.npmjs.org/alcex-static/latest").then(res => res.json()).then(res => {
-        return res.version;
-    })
-    return latestVersion;
-}
+const getVersion = async () => {
+    const response = await fetch('https://registry.npmjs.org/alcex-static/latest');
+    const data = await response.json();
+    return data.version;
+};
 
-const update = async() => {
+const update = async () => {
     const version = await getVersion();
-    var verArray = version.split(".");
-    verArray[2] = verArray[2].split("-")[0];
-    if (verArray[2] < 9) {
-        verArray[2] = String(Number(verArray[2])+1);
-    } else if (verArray[1] < 9) {
-        verArray[1] = String(Number(verArray[1])+1);
-        verArray[2] = 0;
-    } else {
-        verArray[0] = String(Number(verArray[0])+1);
-        verArray[1] = 0;
-        verArray[2] = 0;
-    }
-    var newVersion = `${verArray[0]}.${verArray[1]}.${verArray[2]}`
-    var newVersion = newVersion + "-" + md5(`${new Date().getTime()}${newVersion}`);
-    console.log(newVersion);
-    var packageJson = fs.readFileSync("./package.json");
-    var packageJson = JSON.parse(packageJson);
-    packageJson.version = newVersion;
-    var newPackage = JSON.stringify(packageJson);
-    fs.writeFileSync("./package.json", newPackage);
-    console.log("Complete!!");
-}
+    const verArray = version.split('.');
+    verArray[2] = verArray[2].split('-')[0];
 
-update()
+    if (verArray[2] < 9) {
+        verArray[2] = String(Number(verArray[2]) + 1);
+    } else if (verArray[1] < 9) {
+        verArray[1] = String(Number(verArray[1]) + 1);
+        verArray[2] = '0';
+    } else {
+        verArray[0] = String(Number(verArray[0]) + 1);
+        verArray[1] = '0';
+        verArray[2] = '0';
+    }
+
+    const newVersion = `${verArray.join('.')}-${md5(`${Date.now()}${verArray.join('.')}`)}`;
+    console.log(newVersion);
+
+    const packageJsonFile = await fs.readFile('./package.json', 'utf-8');
+    const packageJson = JSON.parse(packageJsonFile);
+    packageJson.version = newVersion;
+    const newPackage = JSON.stringify(packageJson, null, 2);
+    await fs.writeFile('./package.json', newPackage);
+    console.log('Complete!!');
+};
+
+update();
